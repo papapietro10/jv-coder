62663429c6c366120fe5d5888b1db9652f94a63a
Pedro Duboy <duboy36@gmail.com>
2022-06-04 16:13:00 -0300

    act3

--- js/jv.js
+++ js/jv.js
@@ -156,7 +156,183 @@ else{
 alert('No agregasyte a zapa correcta ')}
 
 
+/*
+const producto1 = (prompt("Ingresa el producto que queres"));
+const zapatilla = 'nike'
+let zapatillaCostoNike1 = 6000
+const zapatilla2 = 'adidas'
+let zapatillaCostoAdidas2 = 9000
+const zapatilla3 = 'puma'
+let zapatillaCostoPuma3 = 2000
 
+for (let i = 0; i <= 1; i++) {
+if (producto1 <= ('') ){
+alert('No agregaste un producto')+ producto1}
+if(queresSeguir === ('si')){
+    producto1 == zapatilla}}
+    if(producto1 === zapatilla){
+(alert(zapatillaCostoNike1))
+queresSeguir = prompt ('Queres seguir comprando?')}
+if(queresSeguir === ('si')){
+producto1 = (prompt("Ingresa el producto que queres"));
+}
+if(producto1 === zapatilla2){
+(alert(zapatillaCostoAdidas2))
+}
+if(queresSeguir === ('si')){
+producto1 = (prompt("Ingresa el producto que queres"));
+}
+
+if(producto1 == zapatilla3){
+costoTotal = zapatillaCostoPuma3;
+alert(costoTotal)
+}
+
+else{
+alert('No agregasyte a zapa correcta ')}
+
+
+
+
+
+
+
+
+
+
+
+
+const tituloDelcitioweb = 'Hola'
+const boTon = '<button type="button" class='
+
+document.write(tituloDelcitioweb);
+
+//let numberUno = 1;
+//let numberTres = 3;
+
+//let resultado = numberUNO+numberTres;
+
+//document.write(resultado);
+
+const textoUno = 'Tu nombre es:';
+const textoDos = prompt('Mi nombre es:');
+
+alert(`Tu nombre es: ${ textoDos}`);
+
+let costoTotal = 0;
+let costoFinal = 0;
+for (let i = 0; i <= 3; i++) {
+let valor1 = parseInt(prompt("Ingrese el costo del primer producto"));
+let valor2 = parseInt(prompt("ingrese el costo del segundo producto"));
+let valor3 = parseInt(prompt("ingrese el costo del tercer producto"));
+costoTotal = valor1 + valor2 + valor3;
+console.log(costoTotal);
+alert("Tus productos tienen un subtotal de $" + costoTotal) 
+costoFinal = costoTotal * 1.21;
+alert("El total de tu compra + IVA es de $" + costoFinal);
+alert("Si desea continuar con la compra, haga click en aceptar");
+break
+}*/
+
+
+/*const edad = 19;
+const tieneEntrada = true;
+const hijoDelDuenio = false;
+
+if((edad >=18 && tieneEntrada) || hijoDelDuenio){
+    console.log("podes entrar")
+}else{
+    console.log("no podes entrar")
+}*/
+
+
+/*
+ if (entradas == oro || String.toUpperCase(oro)){
+alert('Sos parte de la zona vip del evento');
+console.log('Sos parte de la zona vip del evento');
+document.write(bienvenidos);
+console.log(bienvenidos);}
+
+else if ( entradas == plata || (entradas == 'Plata')){
+alert('sos parte de la zona media del evento')
+console.log('sos parte de la zona media del evento')
+document.write(bienvenidos)
+console.log(bienvenidos)}
+
+else if (entradas == bronce || (entradas ==bronce)){
+alert('sosparte de la zona general del evento')
+console.timeLog('sosparte de la zona general del evento')
+document.write(bienvenidos)
+console.log(bienvenidos)
+} 
+else {
+alert("No ingresate una entrada valida");
+console.log("No ingresate una entrada valida")
+entradas = (prompt('Ingrse el color de su entrada:'));
+document.write(soporte)
+console.log(soporte)
+} 
+/*
+if (entradas == 'oro'){
+alert("sos parte de la zona vip del evento");
+console.log('sos parte de la zona vip del evento');
+document.write(bienvenidos);
+console.log(bienvenidos)
+}
+
+if (entradas === 'oro'){
+    console.log('oro');
+    alert("sos parte de la zona vip del evento");
+    console.log('sos parte de la zona vip del evento');
+    document.write(bienvenidos);
+    console.log(bienvenidos)
+} 
+if (entradas === 'plata'){
+    console.log('plata')
+    alert('Sos parte de la zona media del evento')
+    console.log('Sos parte de la zona media del evento')
+    documento
+   }
+*/
+
+
+/*if (tipoDeEntradaUno === 'oro'){
+console.log('Tetoca la zona vip del evento')
+}
+if (tipoDeEntradaDos === 'plata'){
+    console.log('Tetoca la zona media del evento')
+} else{
+console.log('Verifique su entrada en sector de la entrada')
+}*/
+
+/*
+if (entradas <= ('') ){
+    alert('No agregaste una entrada');
+    console.log('No agregaste una entrada');
+    prompt("Ingrse el color de su entrada:");
+    document.write(soporte)
+    }else{
+        alert("ingresa una entrada valida");
+        prompt("Ingrse el color de su entrada:");
+    }
+    */
+
+    /*if (entradas == 'plata' || (entradas == 'Plata')){
+    alert('Sos parte de la zona media del evento');
+    console.log('sos parte de la zona media del evento');
+    document.write(bienvenidos); 
+    console.log('sos parte de la zona media del evento');
+}
+if (entradas == 'bronce'|| (entradas == 'Plata')){
+    alert('Sos parte de la zona general del evento');
+    console.log('Sos parte de la zona general del evento');
+    document.write(bienvenidos);
+}else {
+    alert('no ingresaste un color correcto')
+    entradas = prompt('Ingrse el color de su entrada:')
+}*/
+
+   //location.href = 'http://google.com/search'
 
 
 
